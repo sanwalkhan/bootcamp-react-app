28c517128b15e11e11a34312643c258ce95cd93e
sanwalkhan <sanwalkhan388@yahoo.com>
2021-01-03 13:58:29 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Hello! i`m Usman Tariq.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     Hello World from all of us.
       </header>
     </div>
   );
